19439be374d9455f22104d8cc7c46688243bc30c
Evgenii T <roundcubez@gmail.com>
2026-05-11 00:37:43 +0300

    bill: added phpdocs to models

--- src/Bill/Domain/Model/Bill.php
+++ src/Bill/Domain/Model/Bill.php
@@ -6,6 +6,23 @@ namespace LanBilling\Bill\Domain\Model;
 
 use DateTimeImmutable;
 
+/**
+ * Платеж.
+ *
+ * @property-read int $recordId Идентификатор записи.
+ * @property-read int $accountUid Учетная запись (vg_id из vgroups или uid).
+ * @property-read DateTimeImmutable $payDate Дата платежа.
+ * @property-read ?float $sum Сумма платежа.
+ * @property-read ?int $modPerson Менеджер, модифицировавший запись (person_id из managers).
+ * @property-read ?string $billNum Номер товарной накладной (уникальный идентификатор).
+ * @property-read ?string $billDescr Описание платежа.
+ * @property-read bool $isOrder 0 - обычный платеж, 1 - оплата счета.
+ * @property-read ?int $orderId Идентификатор счета при оплате по счету (замена is_order в 11 сборке).
+ * @property-read ?DateTimeImmutable $remoteDate Дата платежа по часам внешней платежной системы.
+ * @property-read ?DateTimeImmutable $cancelDate Дата аннулирования платежа.
+ * @property-read BillStatus $status Статус платежей, импортированных из внешних систем.
+ * @property-read ?string $receipt Уникальный идентификатор платежа во внешней системе.
+ */
 final readonly class Bill
 {
     public function __construct(
--- src/Bill/Domain/Model/BillStatus.php
+++ src/Bill/Domain/Model/BillStatus.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace LanBilling\Bill\Domain\Model;
 
+/**
+ * Поле bills.status.
+ */
 enum BillStatus
 {
+    /** 0 - первичный платеж */
     case Primary;
+
+    /** 1 - подтвержденный (после сверки) */
     case Confirmed;
+
+    /** 2 - платеж отменен */
     case Cancelled;
 }
