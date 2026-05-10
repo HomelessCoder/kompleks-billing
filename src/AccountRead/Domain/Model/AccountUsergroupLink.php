fc08b4350db9ec94216a28f6cc652c84cfea70a0
Evgenii T <roundcubez@gmail.com>
2026-05-11 00:39:41 +0300

    account-read: added phpdocs to models

--- src/AccountRead/Domain/Model/AccountUsergroupLink.php
+++ src/AccountRead/Domain/Model/AccountUsergroupLink.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace LanBilling\AccountRead\Domain\Model;
 
+/**
+ * Связка пользователя с группой пользователей.
+ *
+ * @property-read int $groupId Идентификатор группы (group_id из usergroups).
+ * @property-read int $uid Идентификатор пользователя (uid из accounts).
+ */
 final readonly class AccountUsergroupLink
 {
     public function __construct(
--- src/AccountRead/Domain/Model/AccountVgroupLink.php
+++ src/AccountRead/Domain/Model/AccountVgroupLink.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace LanBilling\AccountRead\Domain\Model;
 
+/**
+ * Связка пользователя с учетной записью.
+ *
+ * @property-read int $uid Идентификатор пользователя (uid из accounts).
+ * @property-read int $vgId Идентификатор учетной записи (vg_id из vgroups).
+ */
 final readonly class AccountVgroupLink
 {
     public function __construct(
